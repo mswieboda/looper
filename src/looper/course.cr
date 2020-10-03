e9528f6ffc3033774b9de02261e27bc05a770566
Matt Swieboda <mswiebod@gmail.com>
2020-10-03 00:04:46 -0500

    fake rectangle loop course

--- src/looper/course.cr
+++ src/looper/course.cr
@@ -4,11 +4,17 @@ module Looper
 
     def initialize
       @roads = [
-        Road.new(x: 50, y: 50, width: 500, height: 100),
-        Road.new(x: 150, y: 150, width: 100, height: 500)
+        Road.new(x: 100, y: 50, width: 750, height: 100),
+        Road.new(x: 750, y: 100, width: 100, height: 100),
+        Road.new(x: 800, y: 100, width: 100, height: 450),
+        Road.new(x: 750, y: 450, width: 100, height: 100),
+        Road.new(x: 100, y: 500, width: 750, height: 100),
+        Road.new(x: 100, y: 450, width: 100, height: 100),
+        Road.new(x: 50, y: 100, width: 100, height: 450),
+        Road.new(x: 100, y: 100, width: 100, height: 100),
       ]
 
-      @player = Player.new(x: 50, y: 50)
+      @player = Player.new(x: 250, y: 100)
     end
 
     def update(frame_time)
