e9279eff5a10f10d453be66a324e703a1886b72f
Matt Swieboda <mswiebod@gmail.com>
2020-10-04 17:18:03 -0500

    randomize river wave timings

--- src/looper/river_accent.cr
+++ src/looper/river_accent.cr
@@ -1,11 +1,26 @@
 module Looper
   class RiverAccent < Accent
+    DELAY = 5_f32
+
     def initialize(x, y)
       super(x: x, y: y, sprite: Sprite.get(:river_accents))
+      @sprite.frame = @sprite.frames - 1
+      @sprite.pause
+      @delay = rand(DELAY).to_f32
     end
 
     def update(frame_time)
       @sprite.update(frame_time)
+
+      if @sprite.paused? && @delay < DELAY
+        @delay += frame_time
+      elsif @delay >= DELAY
+        @delay = 0_f32
+        @sprite.start
+      elsif @sprite.done?
+        @sprite.pause
+        @delay = rand(DELAY).to_f32
+      end
     end
   end
 end
