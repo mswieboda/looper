a69ed20f32db5741274a9aac38c9eeed5d7dda5d
Matt Swieboda <mswiebod@gmail.com>
2020-10-10 18:06:37 -0500

    start of bigger track

--- src/looper/course.cr
+++ src/looper/course.cr
@@ -53,7 +53,8 @@ module Looper
       @roads << Road.new(x: 275, y: 50, width: 475, height: 137)
       @checkpoints << Checkpoint.new(x: 350, y: 45, width: 50, height: 150, show: true)
 
-      @road_turns << RoadTurn.new(x: 750, y: 600, base: 43)
+      @road_turns << RoadTurn.new(x: 750 + 275, y: 600 - 275, base: 43, degrees: 90, start_degrees: 90)
+      @road_turns << RoadTurn.new(x: 750 + 137, y: 600 - 275, base: 43, degrees: 90, start_degrees: 270)
 
       @roads << Road.new(x: 275, y: 462, width: 475, height: 137)
       @checkpoints << Checkpoint.new(x: 350, y: 455, width: 50, height: 150)
